70f9070a5f57714703ec1b1aab760dc0732e73d0
Trevor Jones <tjones009@yahoo.com>
2020-03-04 20:15:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,15 @@
-# Your Code Here
-def add_numbers(num1, num2)
-  total = num1 + num2
-  yield(total)
-  total
+# Your Code
+
+def map(array)
+  new = []
+  i = 0
+  while i < array.length
+    new << (yield(array[i]))
+    i += 1
+  end
+  new
 end
+
+def reduce (array, sv=nil)
+  if sv
+    sum = sv
